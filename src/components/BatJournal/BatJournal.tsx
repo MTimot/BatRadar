f2df05898fc49b162a2323de8d873373dc89ab83
Kyky Gaming <mesmacquetimothey@gmail.com>
2024-10-29 09:14:53 +0100

    I WANNA PUSH

--- src/components/BatJournal/BatJournal.tsx
+++ src/components/BatJournal/BatJournal.tsx
@@ -1,5 +1,16 @@
 import "./BatJournal.css";
+import { useState } from "react";
+
+const journal = ["cher journal", "blablablabla", "TESTESTEST"];
 function BatJournal() {
+	const [journalIndex, setJournalIndex] = useState(0);
+	const handlePrevious = () => {
+		setJournalIndex(journalIndex - 1);
+	};
+	const handleNext = () => {
+		setJournalIndex(journalIndex + 1);
+	};
+
 	return (
 		<>
 			<div>
@@ -9,14 +20,28 @@ function BatJournal() {
 					autoPlay
 					muted
 					loop
+					id="batvid"
 				/>
 				<div className="batjournal">
-					<p>
-						Cher batjournal, <br />
-						aujourd'hui j'ai fondé mon premier kebab <br />
-						je vends aussi des tacos <br />
-						bisous.
-					</p>
+					<p>{journal[journalIndex]}</p>
+					{journalIndex > 0 ? (
+						<button type="button" onClick={handlePrevious}>
+							Jour précédent
+						</button>
+					) : (
+						<button type="button" disabled>
+							Jour précédent
+						</button>
+					)}
+					{journalIndex < journal.length - 1 ? (
+						<button type="button" onClick={handleNext}>
+							Jour suivant
+						</button>
+					) : (
+						<button type="button" disabled>
+							Jour suivant
+						</button>
+					)}
 				</div>
 			</div>
 		</>
